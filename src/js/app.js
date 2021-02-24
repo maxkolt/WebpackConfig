749cd082ba4492d310d9a3793341a6410cf8197c
Администратор <12345kolt@gmail.com>
2021-02-24 14:11:34 +0300

    Webpack

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
-import { conf as c } from './module1';
-// import * as mod1 from './module1';
-import Product from './module2';
+//import { conf as c } from './module1';
+//import * as mod1 from './module1';
+//import Product from './module2';
 
-console.log(new Product('Apple'));
-console.log(c);
-// console.log(mod1.foo());
+//console.log(new Product('Apple'));
+//console.log(c);
+//console.log(mod1.foo());
+const world = ('hello');
+console.log(world)
